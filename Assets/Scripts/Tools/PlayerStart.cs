12c58cba0b8f3641a788d80d44fbe1a5815fc1cf
alexnava <shinybootzofpleather@gmail.com>
2023-06-05 22:11:28 -0500

    Testing Overworld map. Adding a Stat Manager.

--- Assets/Scripts/Tools/PlayerStart.cs
+++ Assets/Scripts/Tools/PlayerStart.cs
@@ -6,7 +6,6 @@ public class PlayerStart : MonoBehaviour
 {
     public static PlayerStart Instance { get; private set; }
     public GameObject player { get; private set; }
-    public GameObject camera { get; private set; }
 
     private void Awake()
     {
@@ -22,8 +21,7 @@ public class PlayerStart : MonoBehaviour
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        camera = Camera.main.gameObject;//GameObject.FindGameObjectWithTag("Camera");
         player.transform.position = new Vector2(this.transform.position.x, this.transform.position.y - 8f);
-        camera.transform.position = new Vector2(this.transform.position.x, this.transform.position.y + 48f);
+        Camera.main.transform.position = new Vector2(this.transform.position.x, this.transform.position.y + 48f);
     }
 }
