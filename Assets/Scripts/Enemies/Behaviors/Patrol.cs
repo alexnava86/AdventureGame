0551cd1076848b4a7618541fdb25f98327fa135c
alexnava <shinybootzofpleather@gmail.com>
2023-09-06 01:59:14 -0500

    Added WalkLeft/WalkRight animations for Scuttler enemy
    Also added folders for Enemy Behavior scripts

---
+++ Assets/Scripts/Enemies/Behaviors/Patrol.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    [SerializeField]
+    private float time { get; set; }
+    private void OnEnable()
+    {
+        //StartCoroutine(blink(0.075f, 8, new Color(0.9f, 0f, 0f, 1f)));
+    }
+    private IEnumerator Move()
+    {
+        while (time > 0f)
+        {
+            
+            yield return new WaitForSeconds(time);
+        }
+        yield return null;
+    }
+}
