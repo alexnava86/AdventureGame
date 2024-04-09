2915f04c87dc527a9f5e6f2a86d557bd7ebc54fc
Alex Nava <alex.nava.developer@outlook.com>
2024-04-08 23:26:31 -0500

    Update DialogueTree.cs

--- Assets/Scripts/Utility/DialogueTree.cs
+++ Assets/Scripts/Utility/DialogueTree.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+//using SimpleJSON;
 using UnityEngine;
 
 public class DialogueTree //: MonoBehaviour
 {
     public List<DialogueNode> dialogueNodes = new List<DialogueNode>();
+    //private JSONNode json = new JSONNode();
 
-    public void AddDialogueNode(DialogueNode node)
+    public void AddDialogueNode()
     {
+        //Add a Dialogue Node to the current temp dialogue tree
+        DialogueNode node = new DialogueNode();
         dialogueNodes.Add(node);
     }
-    public void RemoveDialogueNode(DialogueNode node)
+    public void RemoveDialogueNode(int nodeID)
     {
-        dialogueNodes.Remove(node);
+        //dialogueNodes.Remove(node);
     }
 }
