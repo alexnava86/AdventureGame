85ca30a02481882cdb37367a1be621228ead6d2d
Alex Nava <alex.nava.developer@outlook.com>
2024-04-10 23:53:52 -0500

    Update DialogueNode.cs

--- Assets/Scripts/Utility/DialogueNode.cs
+++ Assets/Scripts/Utility/DialogueNode.cs
@@ -7,7 +7,7 @@ public class DialogueNode //: MonoBehaviour
     public int NodeID { get; set; }
     public Vector2 NodePosition { get; set; }
     public DialogueNode Parent { get; set; }
-    public List<DialogueNode> Childen { get; set; } //= new List<DialogueNode>();
+    public List<DialogueNode> Children { get; set; } //= new List<DialogueNode>();
     public List<DialogueOption> DialogueOptions { get; set; }
     public string DialogueText { get; set; }
 } 
