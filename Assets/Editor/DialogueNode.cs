43ea500cd72d75e74e2bebe485d37bc1e59ef1ff
Alex Nava <alex.nava.developer@outlook.com>
2024-06-19 01:33:56 -0500

    Create DialogueNode.cs

---
+++ Assets/Editor/DialogueNode.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueNode //: MonoBehaviour
+{
+    public int NodeID { get; set; }
+    public Vector2 NodePosition { get; set; }
+    public DialogueNode Parent { get; set; }
+    public List<DialogueNode> Children { get; set; } //= new List<DialogueNode>();
+    public List<DialogueOption> DialogueOptions { get; set; }
+    public string DialogueText { get; set; }
+} 
